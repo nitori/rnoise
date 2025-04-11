1e7e683c87e8d2e7ec103216cd7d181807096786
Nitori <nitori@chireiden.net>
2025-04-11 23:17:01 +0200

    Add a small comment to explain the different outputs

--- main.py
+++ main.py
@@ -10,4 +10,7 @@ print(f'\n{rust_perlin = }')
 freq = 16
 
 print(f'{py_perlin.noise([1/freq, 2/freq]) = }')
+
+# the rng methods are different, but up until the "vec" is applied in
+# RandVec.get_weighted_val, the values all match with the python version.
 print(f'{rust_perlin.noise([1/freq, 2/freq]) = }')
